6208dbd0c488c6c74b84569bf302085354991a8e
Cihanak1 <250541107@firat.edu.tr>
2025-10-20 12:02:06 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double pay = 9.5*4.5-2.5*3;
+        double payda = 45.5-3.5;
+        double bolum = pay/payda;
+        System.out.printf("İşlem Sonucu = " + "%.4f", bolum);
+    }
+}
