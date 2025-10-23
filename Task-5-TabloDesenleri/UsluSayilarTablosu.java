35adc5dba483075a8b12704b14be092d3587fcdc
Cihanak1 <250541107@firat.edu.tr>
2025-10-23 15:04:45 +0300

    Update UsluSayilarTablosu.java

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,32 @@
+package odev;
+
+public class gorev5_1 {
+
+	public static void main(String[] args) {
+		// Başlık satırı
+        System.out.println("a     a^2     a^3");
+        System.out.println("-----------------");
+
+        // a=1 için
+        int a1 = 1;
+        System.out.println(a1 + "     " + (a1 * a1) + "       " + (a1 * a1 * a1));
+
+        // a=2 için
+        int a2 = 2;
+        System.out.println(a2 + "     " + (a2 * a2) + "       " + (a2 * a2 * a2));
+
+        // a=3 için
+        int a3 = 3;
+        System.out.println(a3 + "     " + (a3 * a3) + "      " + (a3 * a3 * a3));
+
+        // a=4 için
+        int a4 = 4;
+        System.out.println(a4 + "     " + (a4 * a4) + "      " + (a4 * a4 * a4));
+
+        // a=5 için
+        int a5 = 5;
+        System.out.println(a5 + "     " + (a5 * a5) + "     " + (a5 * a5 * a5));
+
+	}
+
+}
