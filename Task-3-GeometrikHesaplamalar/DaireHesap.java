7e81e90527eb52823f03181da333ae3b95480e88
Cihanak1 <250541107@firat.edu.tr>
2025-10-23 15:01:49 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,15 @@
+package odev;
+
+public class gorev3_1 {
+
+	public static void main(String[] args) {
+		double pi = 3.1415;
+		double yarıçap = 5.5;
+		double alan = pi * yarıçap * yarıçap;
+		System.out.println("alan sonucu = " + alan);
+		double çevre = 2 * pi * yarıçap;
+		System.out.println("çevresi = " + çevre);
+
+	}
+
+}
