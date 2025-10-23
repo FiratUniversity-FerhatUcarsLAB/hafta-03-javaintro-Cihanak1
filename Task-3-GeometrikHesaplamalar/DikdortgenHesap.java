0289bbbc70d0a1a6ed84d55c70a5b1c17512efa1
Cihanak1 <250541107@firat.edu.tr>
2025-10-23 15:02:02 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,15 @@
+package odev;
+
+public class gorev3_2 {
+
+	public static void main(String[] args) {
+		double en = 4.5;
+		double boy = 7.9;
+		double alan = en * boy;
+		System.out.println("alan = " + alan);
+		double çevre = 2 * (en + boy);
+		System.out.println("çevresi = " + çevre);
+
+	}
+
+}
