31622e2c49ae2fa0ea8a2eaf41235b3336cbefa0
Cihanak1 <250541107@firat.edu.tr>
2025-10-23 14:59:06 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,33 @@
+package odev;
+
+public class bonus4_1 {
+
+	public static void main(String[] args) {
+		System.out.println("=============================================");
+        System.out.println("            CIHAN           ");
+        System.out.println("=============================================");
+        
+        // Satır 1: C | İ | H | A | N
+        // "İ" harfi için nokta simülasyonu (bir üst satırda da gösterilebilir, burada harfle aynı hizada başlıyor)
+        System.out.println("  CCCC   .      H   H    AAA    N   N");
+        
+        // Satır 2: C | İ | H | A | N
+        System.out.println(" C       I      H   H   A   A   NN  N");
+        
+        // Satır 3: C | İ | H | A | N
+        System.out.println("C        I      H   H   A   A   N N N");
+        
+        // Satır 4: C | İ | H | A | N
+        System.out.println("C        I      HHHHH   AAAAA   N  NN");
+        
+        // Satır 5: C | İ | H | A | N
+        System.out.println(" C       I      H   H   A   A   N   N");
+        
+        // Satır 6: C | İ | H | A | N
+        System.out.println("  CCCC  IIIII   H   H   A   A   N   N");
+        
+        System.out.println("=============================================");
+
+	}
+
+}
