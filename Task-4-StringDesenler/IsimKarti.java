3e23afa979ca7e51adca47150f8da82014432104
Cihanak1 <250541107@firat.edu.tr>
2025-10-23 15:03:12 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,20 @@
+package odev;
+
+public class gorev4_1 {
+
+	public static void main(String[] args) {
+		String ad = "Cihan";
+		String soyad = "Akalın";
+		String ogrNo = "250541107";
+		String bolum = "Yazılım müh.";
+		System.out.println("==================================");
+		System.out.println("Öğrenci Bilgileri");
+		System.out.println("==================================");
+		System.out.println("Ad    : " + ad);
+		System.out.println("Soyad    : " + soyad);
+		System.out.println("Öğrenci no   : " + ogrNo);
+		System.out.println("Bölüm    : " + bolum);
+		
+	}
+
+}
