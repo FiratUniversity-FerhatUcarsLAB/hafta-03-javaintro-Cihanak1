1a26b3c6b305d47c3eec3696a667a71111472a31
Cihanak1 <250541107@firat.edu.tr>
2025-10-23 15:03:51 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,21 @@
+package odev;
+
+public class gorev4_2 {
+
+	public static void main(String[] args) {
+		// 1. çizgi
+		System.out.println("  ******");
+		// 2. çizgi
+		System.out.println(" * *");
+		// 3. satır
+		System.out.println("*");
+		// 4. satır
+		System.out.println("*");
+		// 5. satır
+		System.out.println(" * *");
+		// 6.satır
+		System.out.println("  ******");
+
+	}
+
+}
