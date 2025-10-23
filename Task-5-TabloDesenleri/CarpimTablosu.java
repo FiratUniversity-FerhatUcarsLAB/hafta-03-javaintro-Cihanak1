0a69fba33384492eef4ed49230d9ee227591a946
Cihanak1 <250541107@firat.edu.tr>
2025-10-23 15:05:06 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,43 @@
+package odev;
+
+public class gorev5_3 {
+
+	public static void main(String[] args) {
+		int carpan = 2;
+
+        System.out.println(carpan + " 'nin Çarpım Tablosu (1'den 10'a):");
+        System.out.println("-------------------------");
+
+        // 1. İşlem
+        System.out.printf("%d x  1 =  %2d\n", carpan, carpan * 1);
+
+        // 2. İşlem
+        System.out.printf("%d x  2 =  %2d\n", carpan, carpan * 2);
+
+        // 3. İşlem
+        System.out.printf("%d x  3 =  %2d\n", carpan, carpan * 3);
+
+        // 4. İşlem
+        System.out.printf("%d x  4 =  %2d\n", carpan, carpan * 4);
+
+        // 5. İşlem
+        System.out.printf("%d x  5 =  %2d\n", carpan, carpan * 5);
+
+        // 6. İşlem
+        System.out.printf("%d x  6 =  %2d\n", carpan, carpan * 6);
+
+        // 7. İşlem
+        System.out.printf("%d x  7 =  %2d\n", carpan, carpan * 7);
+
+        // 8. İşlem
+        System.out.printf("%d x  8 =  %2d\n", carpan, carpan * 8);
+
+        // 9. İşlem
+        System.out.printf("%d x  9 =  %2d\n", carpan, carpan * 9);
+
+        // 10. İşlem
+        System.out.printf("%d x 10 = %2d\n", carpan, carpan * 10);
+
+	}
+
+}
